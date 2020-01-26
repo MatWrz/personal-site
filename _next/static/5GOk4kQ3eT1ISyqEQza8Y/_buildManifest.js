0549b2779730c943926c8d7dc29b0797926f772b
MatWrz <MatWrz@users.noreply.github.com>
2020-01-26 23:19:58 +0000

    Automated deployment: Sun Jan 26 23:19:58 UTC 2020 53343f8fcf7dc9c44f67588aec8f3814c289748a

--- _next/static/5GOk4kQ3eT1ISyqEQza8Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/cv":[a,b],"/index":[a,b]}}("static\u002Fcss\u002Ff414066cbd5c3cb0fd2ba53a46b9c34dbb74436d.9e6748a6.chunk.css","static\u002Fchunks\u002Ff414066cbd5c3cb0fd2ba53a46b9c34dbb74436d.eb7dbab5a8359a61b996.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
